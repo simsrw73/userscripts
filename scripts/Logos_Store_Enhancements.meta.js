b1c84ff579cd3c0383c96bda2165c9f6a5ec660c
Randy W. Sims <simsrw73@gmail.com>
2019-05-22 09:01:22 -0400

    Add timeout and sanity checks

---
+++ scripts/Logos_Store_Enhancements.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Logos Store Enhancements
+// @namespace    https://github.com/simsrw73/userscripts
+// @version      0.1
+// @description  Get extended information about resources
+// @author       Randy W. Sims
+// @updateURL    https://github.com/simsrw73/userscripts/raw/master/scripts/Logos_Store_Enhancements.meta.js
+// @downloadURL  https://github.com/simsrw73/userscripts/raw/master/scripts/Logos_Store_Enhancements.user.js
+// @match        https://beta.logos.com/products/*
+// @run-at       document-end
+// ==/UserScript==
--- scripts/Logos_Store_Enhancements.user.js
+++ scripts/Logos_Store_Enhancements.user.js
@@ -1,9 +1,11 @@
 // ==UserScript==
 // @name         Logos Store Enhancements
 // @namespace    https://github.com/simsrw73/userscripts
-// @version      0.1
+// @version      0.2
 // @description  Get extended information about resources
 // @author       Randy W. Sims
+// @updateURL    https://github.com/simsrw73/userscripts/raw/master/scripts/Logos_Store_Enhancements.meta.js
+// @downloadURL  https://github.com/simsrw73/userscripts/raw/master/scripts/Logos_Store_Enhancements.user.js
 // @match        https://beta.logos.com/products/*
 // @run-at       document-end
 // ==/UserScript==
@@ -12,28 +14,36 @@
   'use strict';
 
   // Show resource ID and link to open it
-  var displayRIDWhenRendered = setInterval(function() {
-    var seeInside = document.querySelector('.core-see-inside__iframe');
+  let displayRIDWhenRendered_limit = 10;
+  const displayRIDWhenRendered = setInterval(function() {
+    // Timeout if we've been through this too many times before
+    if (--displayRIDWhenRendered_limit <= 0) {
+      clearInterval(displayRIDWhenRendered);
+    }
+
+    const seeInside = document.querySelector('.core-see-inside__iframe');
     if (seeInside !== null) {
       clearInterval(displayRIDWhenRendered);
 
       const srcURL = new URL(seeInside.getAttribute('src'));
       const resourceName = srcURL.searchParams.get('resourceName');
 
-      const bookURL =
-        'https://app.logos.com/books/' + encodeURIComponent(resourceName);
+      if (resourceName !== null) {
+        const bookURL =
+              'https://app.logos.com/books/' + encodeURIComponent(resourceName);
 
-      const a = document.createElement('a');
-      const aText = document.createTextNode(resourceName.substring(4)); // remove LLS:
-      a.appendChild(aText);
-      a.title = 'Resource Name';
-      a.className = 'btn btn-minor';
-      a.href = bookURL;
-      a.style.marginBottom = '8px';
+        const a = document.createElement('a');
+        const aText = document.createTextNode(resourceName.substring(4)); // remove LLS:
+        a.appendChild(aText);
+        a.title = 'Resource Name';
+        a.className = 'btn btn-minor';
+        a.href = bookURL;
+        a.style.marginBottom = '8px';
 
-      const seeInsideBtn = document.querySelector('.btn-see-inside');
-      seeInsideBtn.parentNode.style.flexDirection = 'column';
-      seeInsideBtn.parentNode.insertBefore(a, seeInsideBtn);
+        const seeInsideBtn = document.querySelector('.btn-see-inside');
+        seeInsideBtn.parentNode.style.flexDirection = 'column';
+        seeInsideBtn.parentNode.insertBefore(a, seeInsideBtn);
+      }
     }
   }, 500);
 })();
