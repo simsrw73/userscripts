4ae90aeb336fa5faf0f1f6f95af9b1130b756e8e
Randy W. Sims <simsrw73@gmail.com>
2019-05-28 23:01:04 -0400

    Add share to Faithlife

--- scripts/Logos_Store_Enhancements.user.js
+++ scripts/Logos_Store_Enhancements.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Logos Store Enhancements
 // @namespace    https://github.com/simsrw73/userscripts
-// @version      0.4.1
+// @version      0.4.2
 // @description  Get extended information about resources
 // @author       Randy W. Sims
 // @updateURL    https://github.com/simsrw73/userscripts/raw/master/scripts/Logos_Store_Enhancements.meta.js
@@ -100,7 +100,7 @@
   const sidebar = document.querySelector('div[class^="index--desktopSidebarContainer"]');
   if (sidebar !== null) {
     const to = document.createElement('div');
-    to.className = 'lseShareTo';
+    to.id = 'lseSocialBar';
 
     addPageNav(to);
     addSocialIcons(to);
@@ -113,7 +113,7 @@
     const nav = document.createElement('div');
     nav.id = 'lseNavTo';
     nav.style.position = 'relative';
-    nav.minWidth = '118px';
+    nav.style.minWidth = '118px';
 
     nav.addEventListener('mouseenter', function(e) {
       const toc = document.querySelector('#lseNavTo--TOC');
@@ -171,6 +171,10 @@
   function addSocialIcons(to) {
     const url = document.head.querySelector("[property='og:url'][content]").content;
     const title = document.head.querySelector("[property='og:title'][content]").content;
+    let flURL = 'https://faithlife.com/share?content=' +
+      encodeURIComponent(title) +
+      '&url=' +
+      encodeURIComponent(url);
     let fbURL =
       'https://www.facebook.com/sharer/sharer.php?kid_directed_site=0&sdk=joey&u=' +
       encodeURIComponent(url) +
@@ -193,11 +197,30 @@
     p.appendChild(aText);
     social.appendChild(p);
 
-    // Facebook
+    // Faithlife
     let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    svg.setAttributeNS(null, 'viewBox', '0 0 24 24');
+    svg.setAttributeNS(null, 'width', 20);
+    svg.setAttributeNS(null, 'height', 20);
+    svg.style.display = 'inline-block';
+    svg.style.marginLeft = '8px';
+    svg.insertAdjacentHTML(
+      'beforeend',
+      '<g id="g3739" transform="matrix(0.61790844,0,0,0.61790844,-0.36363799,-0.2920958)"><circle r="19.420353" cy="19.893066" cx="20.008844" id="path24" style="fill:currentColor;fill-opacity:1;stroke-width:0.68275863" /><g transform="translate(-37.266609,0.16343591)" id="g30"><path style="fill:#ffffff;fill-rule:evenodd;stroke-width:0.91120678" inkscape:connector-curvature="0" id="path6" d="m 58.521238,37.083593 c -0.01822,0.04738 -0.08019,0.05923 -0.114812,0.02095 -0.478384,-0.5285 -3.268499,-3.521813 -7.439093,-6.360222 -2.393739,-1.694845 -3.331371,-2.745466 -3.602911,-3.085347 -0.03645,-0.04556 0,-0.114812 0.05832,-0.112078 0.277919,0.01549 0.923964,0.03918 1.740406,-0.01458 0.05467,-0.0037 0.08201,-0.06197 0.05467,-0.106611 -0.350815,-0.537612 -2.03837,-3.225672 -2.15956,-6.232654 -0.0018,-0.05467 0.05467,-0.08929 0.102051,-0.06379 0.255138,0.145793 0.953122,0.512098 1.84246,0.776348 0.05012,0.01458 0.0975,-0.03007 0.08656,-0.08019 -0.133036,-0.625999 -0.72532,-3.768751 -0.125746,-6.95433 0.01094,-0.05649 0.08019,-0.07655 0.118457,-0.03645 0.177686,0.196821 0.624176,0.633288 1.409637,1.089803 0.03645,0.02187 0.08201,0.0054 0.0975,-0.03645 0.191353,-0.548546 1.406903,-3.881741 3.459852,-5.880928 0.04009,-0.039184 0.107522,-0.014579 0.114812,0.041 0.898449,5.891862 5.98845,7.234981 8.394947,12.424304 1.614659,3.48081 -0.430089,5.394343 -1.10256,5.904619 -0.05649,0.04101 -0.02278,0.130303 0.04739,0.123925 3.840736,-0.309811 5.430792,-3.79062 5.847213,-4.89956 0.02734,-0.07107 0.131213,-0.05285 0.133947,0.02278 0.268807,8.206327 -6.394848,8.692912 -7.451848,8.724804 -0.08838,0.0018 -0.176775,-0.01822 -0.255138,-0.06014 -6.024899,-3.212003 -5.995741,-9.613231 -5.933779,-10.825136 0.0018,-0.03645 -0.05012,-0.0483 -0.06379,-0.01367 -3.028852,7.320635 2.185074,11.457514 4.043024,13.26717 0.744456,0.72532 1.073402,1.36681 0.695251,2.367315" /><path style="fill:#ffffff;fill-rule:evenodd;stroke-width:0.91120678" inkscape:connector-curvature="0" id="path8" d="m 59.437912,2.3575055 c 0.04829,-0.054673 0.139414,-0.015489 0.130302,0.058315 -0.136681,1.1180508 -0.444669,5.8289897 3.328638,9.9230405 4.241667,4.569701 3.781509,8.055068 2.268905,11.291673 -0.03462,0.07289 -0.145793,0.0483 -0.14306,-0.03371 0.02734,-0.872936 -0.209577,-2.678948 -3.161886,-5.943802 C 57.18632,12.486476 53.908709,8.6831009 59.437,2.3556813" /></g></g>'
+    );
+
+    let a = document.createElement('a');
+    a.appendChild(svg);
+    a.href = flURL;
+    a.style.color = '#333'; // '#3b5998';
+
+    social.appendChild(a);
+
+    // Facebook
+    svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
     svg.setAttributeNS(null, 'viewBox', '0 0 512 512');
-    svg.setAttributeNS(null, 'width', 24);
-    svg.setAttributeNS(null, 'height', 24);
+    svg.setAttributeNS(null, 'width', 20);
+    svg.setAttributeNS(null, 'height', 20);
     svg.style.display = 'inline-block';
     svg.style.marginLeft = '8px';
     svg.insertAdjacentHTML(
@@ -205,7 +228,7 @@
       '<path fill="currentColor" d="M504 256C504 119 393 8 256 8S8 119 8 256c0 123.78 90.69 226.38 209.25 245V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.28c-30.8 0-40.41 19.12-40.41 38.73V256h68.78l-11 71.69h-57.78V501C413.31 482.38 504 379.78 504 256z"></path>'
     );
 
-    let a = document.createElement('a');
+    a = document.createElement('a');
     a.appendChild(svg);
     a.href = fbURL;
     a.style.color = '#333'; // '#3b5998';
@@ -215,8 +238,8 @@
     // Twitter
     svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
     svg.setAttributeNS(null, 'viewBox', '0 0 512 512');
-    svg.setAttributeNS(null, 'width', 24);
-    svg.setAttributeNS(null, 'height', 24);
+    svg.setAttributeNS(null, 'width', 20);
+    svg.setAttributeNS(null, 'height', 20);
     svg.style.display = 'inline-block';
     svg.style.marginLeft = '8px';
     svg.insertAdjacentHTML(
@@ -256,15 +279,19 @@
 
   ///////////////   CSS
   const css = [
-    '.lseShareTo {',
+    '#lseSocialBar {',
     '  display: flex;',
     '  flex-direction: row;',
     '  justify-content: space-between;',
+    '  align-items: baseline;',
     '  padding: 8px 8px 8px 0;',
+    '}',
+    '#lseShareTo {',
+    '  display: flex;',
     '}',
     '#lseShareTo--Label {',
     '  margin-left: 16px;',
-    '  font-size: 18px;',
+    '  font-size: 16px;',
     '  vertical-align: middle;',
     '  display: inline-block;',
     '  user-select: none;',
