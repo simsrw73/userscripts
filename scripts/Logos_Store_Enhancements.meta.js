a87a8cec4188239c8b2d2da0e942d46c865c420c
Randy W. Sims <simsrw73@gmail.com>
2019-05-31 15:08:21 -0400

    Run on all Faithlife book stores

--- scripts/Logos_Store_Enhancements.meta.js
+++ scripts/Logos_Store_Enhancements.meta.js
@@ -1,11 +1,16 @@
 // ==UserScript==
 // @name         Logos Store Enhancements
 // @namespace    https://github.com/simsrw73/userscripts
-// @version      0.4.7
+// @version      0.4.8
 // @description  Get extended information about resources
 // @author       Randy W. Sims
 // @updateURL    https://github.com/simsrw73/userscripts/raw/master/scripts/Logos_Store_Enhancements.meta.js
 // @downloadURL  https://github.com/simsrw73/userscripts/raw/master/scripts/Logos_Store_Enhancements.user.js
-// @match        https://beta.logos.com/products/*
+// @match        https://*.logos.com/*
+// @match        https://verbum.com/*
+// @match        https://ebooks.faithlife.com/*
+// @match        https://ebooks.noet.com/*
+// @grant        GM_xmlhttpRequest
+// @connect      libapi.logos.com
 // @run-at       document-end
 // ==/UserScript==
